418b896b92d65c04df4e8fde350bb561566798c0
Sanyajo <sakhatskiy2016@mail.ru>
2023-06-11 18:33:30 +0300

    exercise_1

---
+++ 1_Basics_of_software_code_development/exercise_1/Exercise_1/main1.java
@@ -0,0 +1,18 @@
+package Exercise_1;
+import java.util.Scanner;
+
+public class main1{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        short a  = in.nextShort();
+        short b  = in.nextShort();
+        short c  = in.nextShort();
+        System.out.println(function(a,b,c));
+        in.close();
+    }
+
+    static int function(short a, short b, short c){
+        int z =( (a-3) * b/2) + c;
+        return z;
+    }
+}
