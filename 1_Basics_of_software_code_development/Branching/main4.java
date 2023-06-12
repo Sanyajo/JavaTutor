bc0f61916df1f2b56845895ceb25286ba38fc731
Sanyajo <sakhatskiy2016@mail.ru>
2023-06-12 13:48:47 +0300

    exercise_4

---
+++ 1_Basics_of_software_code_development/Branching/main4.java
@@ -0,0 +1,11 @@
+public class main4 {
+    public static void main (String[] args){
+      int A = 4, B = 3 ;
+      int x = 3, y = 2, z = 2;
+      if ( z < A && y < B){
+          System.out.println("Пройдет");
+      }else{
+          System.out.println("Не пройдет");
+      }
+    }
+}
