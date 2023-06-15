535af74a35b3e79bd6fb06f4fd9249b26da75843
Sanyajo <sakhatskiy2016@mail.ru>
2023-06-15 20:35:36 +0300

    exercise_6

--- 2_Algorithmization/array array/main6.java
+++ 2_Algorithmization/array array/main6.java
@@ -2,28 +2,27 @@ import java.util.Scanner;
 
 public class main6 {
     public static void main(String[] args) {
-        int row  = 6;
-        final int  column = 6;
+        int N  = 6;
         Scanner in = new Scanner(System.in);
-        int[][] arr = new int [row][row];
+        int[][] arr = new int [N][N];
 
-        for(int i = 0 ; i < (row/2 + row%2); ++i){
-            for(int j = 0; j < row; ++j){
-                if(j < i || j > row-(i+1) ){
-                    arr[row-i-1][j] = 0;
+        for(int i = 0 ; i < (N/2 + N%2); ++i){
+            for(int j = 0; j < N; ++j){
+                if(j < i || j > N-(i+1) ){
+                    arr[N-i-1][j] = 0;
                     arr[i][j] = 0;
                 }else{
-                    arr[row-i-1][j] = 1;
+                    arr[N-i-1][j] = 1;
                     arr[i][j] = 1;
                 }
             }
         }
-        prt(arr,row,column);
+        prt(arr,N);
     }
 
-    public static void prt(int[][] arr, int row, int column){
+    public static void prt(int[][] arr, int row){
         for(int i =0; i < row; ++i){
-            for(int j = 0; j < column; ++j){
+            for(int j = 0; j < row; ++j){
                 System.out.print(arr[i][j]+ " ");
             }
             System.out.println();
