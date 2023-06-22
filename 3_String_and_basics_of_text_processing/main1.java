60e4a4f7ba78448be25cab742b530c82ca97d293
Sanyajo <sakhatskiy2016@mail.ru>
2023-06-22 16:53:38 +0300

    exercise_1

--- 3_String_and_basics_of_text_processing/main1.java
+++ 3_String_and_basics_of_text_processing/main1.java
@@ -1,5 +1,21 @@
-package 3_String_adn_basics_of_text_processing;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class main1 {
-    
+
+    public static void main(String[] args) {
+       String[] str = {"HelloWorld","MyName"};
+       Pattern pattern = Pattern.compile("\\B([A-Z])");
+       for(int i=0; i <str.length; ++i){
+           StringBuffer strb = new StringBuffer();
+           Matcher matcher = pattern.matcher(str[i]);
+           while(matcher.find()){
+               matcher.appendReplacement(strb, "_$0");
+           }
+           matcher.appendTail(strb);
+           str[i] = strb.toString().toLowerCase();
+       }
+       System.out.println(Arrays.toString(str));
+    }
 }
