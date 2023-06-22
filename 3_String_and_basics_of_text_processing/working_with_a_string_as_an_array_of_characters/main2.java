912e093a1fc5551372e0b1cc26cc8fceb8dbaf41
Sanyajo <sakhatskiy2016@mail.ru>
2023-06-22 16:56:38 +0300

    exercise_2

---
+++ 3_String_and_basics_of_text_processing/working_with_a_string_as_an_array_of_characters/main2.java
@@ -0,0 +1,9 @@
+public class main2 {
+
+    public static void main(String[] args) {
+        String str = "Hello world word";
+        str = str.replace("word","letter");
+        System.out.println(str);
+
+    }
+}
